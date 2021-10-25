ec53d7d7ddb056e2ff7328c7dd125c2c4a8c2b6b
Parth Sawant <parthsawant97@gmail.com>
2021-10-25 22:53:49 +0530

    checking

---
+++ prac2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define INF INT_MAX
+#define rep(i, n) for(int i=0;i<n;i++) 
+typedef pair<ll,ll> ppi;
+typedef vector<int> vi;
+typedef pair<int,string> pis;
+
+/*
+
+ 
+  
+*/
+
+void solve() {
+  cout << ":=hi";
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+#endif
+
+
+  solve();
+
+    return 0;
+}
+
+
